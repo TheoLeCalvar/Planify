f8e6611f54d89d2f4735b7d9398f8dac784b7123
Sondama <sonda.makni@etudiant-enit.utm.tn>
2023-01-05 22:06:08 +0100

    front commit

---
+++ front-end/PlanningFront/src/app/rtaf-form/rtaf-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rtaf-form',
+  templateUrl: './rtaf-form.component.html',
+  styleUrls: ['./rtaf-form.component.css']
+})
+export class RtafFormComponent {
+
+}
