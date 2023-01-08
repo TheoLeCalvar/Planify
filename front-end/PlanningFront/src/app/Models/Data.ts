e669eb7b638da95f8a8b41ac9e7ac0fd9b4b73b4
Sondama <sonda.makni@etudiant-enit.utm.tn>
2023-01-08 13:26:51 +0100

    add models

---
+++ front-end/PlanningFront/src/app/Models/Data.ts
@@ -0,0 +1,9 @@
+import { Module } from "./Module";
+
+export class Data {
+    weeksNumber: number;
+    modulesUeA: [Module];
+    modulesUeB: [Module];
+    modulesUeC: [Module];
+    unavailable : [String] 
+}
--- front-end/PlanningFront/src/app/rtaf-form/rtaf-form.component.ts
+++ front-end/PlanningFront/src/app/rtaf-form/rtaf-form.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
+import { Module } from '../Models/Module';
 
-interface Module {
-    id: number;
-    name: string;
-    nbCreneau: number
-}
+
 
 @Component({
   selector: 'app-rtaf-form',
@@ -14,29 +11,60 @@ interface Module {
 export class RtafFormComponent {
     selectedNumberWeek: number =0 ;
     selectedNumberModuleUEA : number =0;
+    selectedNumberModuleUEB : number =0;
+    selectedNumberModuleUEC : number =0;
 
-    modulesUEA: Module[] = [
-        {
-            id: 1,
-            name: "UEA-1",
-            nbCreneau: 5
-        },
-        {
-            id: 2,
-            name: "UEA-2",
-            nbCreneau: 6
-        }
-    ];
+    modulesUEA: Module[] = [];
+    modulesUEB: Module[] = [];
+    modulesUEC: Module[] = [];
 
     selectWeekChange() {
         console.log(this.selectedNumberWeek);
     }
     selectModuleUEAChange(){
         console.log(this.selectedNumberModuleUEA);
+        this.modulesUEA = [];
+        for(let i=1; i <= this.selectedNumberModuleUEA ; i++){
+            this.modulesUEA.push({
+                id: i,
+                name: "",
+                nbCreneau: 0
+            })
+        }
        
     }
-    log(index: number) {
-        console.log(index);
+
+    selectModuleUEBChange(){
+        console.log(this.selectedNumberModuleUEB);
+        this.modulesUEB = [];
+        for(let i=1; i <= this.selectedNumberModuleUEB ; i++){
+            this.modulesUEB.push({
+                id: i,
+                name: "",
+                nbCreneau: 0
+            })
+        }
+       
+    }
+
+    selectModuleUECChange(){
+        console.log(this.selectedNumberModuleUEC);
+        this.modulesUEC = [];
+        for(let i=1; i <= this.selectedNumberModuleUEC ; i++){
+            this.modulesUEC.push({
+                id: i,
+                name: "",
+                nbCreneau: 0
+            })
+        }
+    }
+
+    clickFunction() {
+        console.log("number of week :",this.selectedNumberWeek);
+        console.log("modules UEA",this.modulesUEA);
+        console.log("modules UEB",this.modulesUEB);
+        console.log("modules UEC",this.modulesUEC);
+
     }
 
 }
