2167828d2f57cb695704081106913b0b0329b0d4
Sondama <sonda.makni@etudiant-enit.utm.tn>
2023-01-08 13:45:09 +0100

    set data service

---
+++ front-end/PlanningFront/src/app/Services/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import{HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Data } from '../Models/Data';
+
+let config = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': "*",
+    'Access-Control-Allow-Headers': "*",
+    'Access-Control-Allow-Credentials': 'true'
+    })
+  }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  baseUrl : string = "http://localhost:3200/data/solver";
+
+  constructor(private http:HttpClient) { }
+
+  sendData(data: Data):Observable<Data>{
+    //send body with request
+    return this.http.post<Data>(this.baseUrl, data, config);
+  }
+}
